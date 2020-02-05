b7ad517892d56ea2df9e1ac44d44999548370718
Rytautas <rytautas.kvasinskas@gmail.com>
2020-02-05 15:33:16 +0200

    conflicting change from master

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("change 1");
+        System.out.println("conflicting change from master");
     }
 }
